c2a6affda8bc99354b4a5ac27440748ae7567a9d
Raymond Ativie <RaymondAtivie@gmail.com>
2024-01-02 14:20:45 +0000

    fix login link

--- src/components/Dashboard..vue
+++ src/components/Dashboard..vue
@@ -1,24 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="relative lg:h-[50rem] py-12 text-white text-center">
-    <img
-      class="absolute top-0 left-0 object-top h-full w-full"
-      src="../assets/bg-layer.png"
-      alt="bg layer"
-    />
-    <div class="max-w-2xl mx-auto relative z-10">
-      <h2 class="mb-6 text-3xl font-bold">Desktop Dashboard</h2>
-      <p class="mb-6">
-        We have included a desktop dashboard for easy control monitoring of
-        multiple vehicles and fire locations.
-      </p>
-      <button
-        class="mb-6 py-3 px-6 rounded-md text-black bg-white hover:bg-brand-orange-1 hover:text-white"
-      >
-        <a href="https://v2.firealert.mobi/login">Login</a>
-      </button>
-      <img src="../assets/dashboard.png" alt="Dashboard" />
-    </div>
-  </section>
+	<section class="relative lg:h-[50rem] py-12 text-white text-center">
+		<img class="absolute top-0 left-0 object-top h-full w-full" src="../assets/bg-layer.png" alt="bg layer" />
+		<div class="max-w-2xl mx-auto relative z-10">
+			<h2 class="mb-6 text-3xl font-bold">Desktop Dashboard</h2>
+			<p class="mb-6">We have included a desktop dashboard for easy control monitoring of multiple vehicles and fire locations.</p>
+			<a href="https://v2.firealert.mobi/login">
+				<button class="mb-6 py-3 px-6 rounded-md text-black bg-white hover:bg-brand-orange-1 hover:text-white">Login</button>
+			</a>
+			<img src="../assets/dashboard.png" alt="Dashboard" />
+		</div>
+	</section>
 </template>
--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="bg-brand-blue-1">
-    <div
-      class="max-w-7xl mx-auto px-4 py-6 flex items-center justify-between text-white"
-    >
-      <img class="w-16 h-16" src="../assets/logo.png" alt="Logo" />
-      <ul class="hidden md:flex items-center gap-6">
-        <a class="hover:text-brand-orange-2" href="#about"><li>About</li></a>
-        <a class="hover:text-brand-orange-2" href="#application"
-          ><li>Application</li></a
-        >
-        <a class="hover:text-brand-orange-2" href="#work"
-          ><li>How does it work?</li></a
-        >
-        <a class="hover:text-brand-orange-2" href="#features"
-          ><li>Features</li></a
-        >
-      </ul>
-      <div class="hidden md:flex items-center gap-6">
-        <button
-          class="py-3 px-6 rounded-md bg-white text-black hover:bg-brand-orange-1 hover:text-white"
-        >
-          Download
-        </button>
-        <button
-          class="py-3 px-6 rounded-md bg-brand-orange-2 text-white hover:bg-brand-orange-1"
-        >
-          <a href="https://v2.firealert.mobi/login">Log in</a>
-        </button>
-      </div>
-    </div>
-  </nav>
+	<nav class="bg-brand-blue-1">
+		<div class="max-w-7xl mx-auto px-4 py-6 flex items-center justify-between text-white">
+			<img class="w-16 h-16" src="../assets/logo.png" alt="Logo" />
+			<ul class="hidden md:flex items-center gap-6">
+				<a class="hover:text-brand-orange-2" href="#about"><li>About</li></a>
+				<a class="hover:text-brand-orange-2" href="#application"><li>Application</li></a>
+				<a class="hover:text-brand-orange-2" href="#work"><li>How does it work?</li></a>
+				<a class="hover:text-brand-orange-2" href="#features"><li>Features</li></a>
+			</ul>
+			<div class="hidden md:flex items-center gap-6">
+				<button class="py-3 px-6 rounded-md bg-white text-black hover:bg-brand-orange-1 hover:text-white">Download</button>
+				<a href="https://v2.firealert.mobi/login">
+					<button class="py-3 px-6 rounded-md bg-brand-orange-2 text-white hover:bg-brand-orange-1">Log in</button>
+				</a>
+			</div>
+		</div>
+	</nav>
 </template>
